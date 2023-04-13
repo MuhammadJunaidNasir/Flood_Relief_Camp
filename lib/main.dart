592420c0f22f442aaa48b9df0f7d11acdd0d76b3
Muhammad Junaid Nasir <mjn7439@gmail.com>
2023-04-13 05:59:53 +0500

    My Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  Stripe.publishableKey='pk_test_51Mn562FT4JdnQJIkoT2q91T7npTKx78byYr7V0em8mc5C0MMH2M03RrzHzxiKiS82Zb8SOxC9WIBEHoCoM69slmw00KkJF20sV';
-  runApp( const MyApp());
+  //Stripe
+  Stripe.publishableKey =
+      'pk_test_51Mn562FT4JdnQJIkoT2q91T7npTKx78byYr7V0em8mc5C0MMH2M03RrzHzxiKiS82Zb8SOxC9WIBEHoCoM69slmw00KkJF20sV';
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
